6fcd66305467cc425d2a58076a8ad8f79e2fa175
Nicholas Lum <nicholaslum444@hotmail.com>
2016-09-20 01:11:45 +0800

    john cena

--- compclubbot.rb
+++ compclubbot.rb
@@ -4,26 +4,35 @@ require 'nokogiri'
 require 'open-uri'
 
 token = YAML::load(IO.read('./secrets.yml'))['telegram_bot_token']
-welcomeMessage = "Welcome to Computing Club Bot.\n\nUse /getblast to get the latest email blast message."
+welcomeMessage = "This bot gives useful information about CompClub:\n/getblast - for the latest email blast.\n/help - for this message."
 errorMessage = "Error retrieving email blast. Please try again later."
 unknownCmdMessage = "Unknown command. Please try again."
 
 Telegram::Bot::Client.run(token, logger: Logger.new($stderr)) do |bot|
     bot.listen do |message|
         if message.text == nil
-            bot.api.send_message(chat_id: message.chat.id,
-                                 text: welcomeMessage
-                                )
+            # do nothing
         else
             messageSplit = message.text.split
             cmd = messageSplit[0]
 
             case cmd
-            when '/start'
+            #--------------------
+            # COMMANDS: /start, /help
+            when '/start', '/help', '/start@CompClubBot', '/help@CompClubBot'
                 bot.api.send_message(chat_id: message.chat.id,
                                      text: welcomeMessage
                                     )
 
+            #--------------------
+            # COMMANDS: /johncena
+            when '/johncena', '/johncena@CompClubBot'
+                bot.api.send_photo(chat_id: message.chat.id,
+                                   photo: Faraday::UploadIO.new('img/cena.jpg', 'image/jpeg')
+                                  )
+
+            #---------------------
+            # COMMANDS: /getblast, /gb
             when '/getblast', '/gb', '/getblast@CompClubBot', '/gb@CompClubBot'
                 if messageSplit.length == 1 || messageSplit[1] == 'latest'
                     bot.api.send_message(chat_id: message.chat.id,
@@ -52,11 +61,6 @@ Telegram::Bot::Client.run(token, logger: Logger.new($stderr)) do |bot|
                                          text: errorMessage
                                         )
                 end
-
-            else
-                bot.api.send_message(chat_id: message.chat.id, 
-                                     text: welcomeMessage
-                                    )
             end
         end
     end
